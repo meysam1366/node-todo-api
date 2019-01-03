51b4759821b75d04875c63b698adfcd3acc87f9e
meysam maghsoudi <meysammaghsoudi@meysams-MacBook-Pro.local>
2019-01-03 22:46:28 +0330

    Hashing password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
-const {SHA256} = require('crypto-js');
+const {
+    SHA256
+} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-}
+var password = "123abc!";
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$W.X50VYMDcBkes2XBZonyO9X7.bwlFC3ZsB3QVCFiY4gjQWcmF3Sy';
+bcrypt.compare(password, hashedPassword, (err, result) => {
+    console.log(result);
+});
+
+// var data = {
+//     id: 10
+// }
 
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 
-var decoded = jwt.decode(token,'123abc');
-console.log("Decode ",decoded);
+// var decoded = jwt.decode(token, '123abc');
+// console.log("Decode ", decoded);
 
 // var message = "I am user number 3";
 // var hash = SHA256(message).toString();
